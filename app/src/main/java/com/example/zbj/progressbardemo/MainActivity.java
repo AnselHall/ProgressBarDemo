eb3fff3937a47b7181305bb299ed71cac1c186d4
youzhiqingnian <359455763@qq.com>
2016-05-24 17:04:08 +0800

    1

--- app/src/main/java/com/example/zbj/progressbardemo/MainActivity.java
+++ app/src/main/java/com/example/zbj/progressbardemo/MainActivity.java
@@ -1,18 +1,52 @@
 package com.example.zbj.progressbardemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.SeekBar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.zbj.progressbardemo.widget.RangeSeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_min;
+    private TextView tv_max;
+    private RangeSeekBar rsb_range;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        SeekBar sb = (SeekBar) findViewById(R.id.sb_value);
-//
-//        sb.getMax();
+        initViews();
+
+        registerListener();
+    }
+
+    private void registerListener() {
+
+        rsb_range.setNotifyWhileDragging(true);
+        rsb_range.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener() {
+            @Override
+            public void onRangeSeekBarValuesChanged(RangeSeekBar bar, Number minValue, Number maxValue) {
+
+                int min = minValue.intValue();
+                int max = maxValue.intValue();
+
+                if (min == 50 && max == 5000) {
+                    tv_min.setText("¥" + min);
+                    tv_max.setText("¥5000+");
+                    return;
+                }
+
+                tv_min.setText("¥" + min);
+                tv_max.setText("¥" + max);
+            }
+        });
+    }
+
+    private void initViews() {
+        tv_min = (TextView) findViewById(R.id.tv_min);
+        tv_max = (TextView) findViewById(R.id.tv_max);
+        rsb_range = (RangeSeekBar) findViewById(R.id.rsb_range);
     }
 }
